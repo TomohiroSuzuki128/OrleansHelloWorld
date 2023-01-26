87ec8ab907a8423c8431060e707d4283a34bfd60
Tomohiro Suzuki <suzuchan@muf.biglobe.ne.jp>
2023-01-26 23:33:14 +0900

    [WIP] Add Sample App using Azure Table Storage and App Service

---
+++ Grains/Status/WriteLargeDataState.cs
@@ -0,0 +1,11 @@
+﻿namespace Grains.Status
+{
+    [Serializable]
+    public class ValidZipCodeState
+    {
+        public ValidZipCodeState() { }
+        public ValidZipCodeState(string zipCode) => ValidZipCodes.Add(zipCode);
+        public List<string> ValidZipCodes { get; private set; } = new();
+    }
+}
+
